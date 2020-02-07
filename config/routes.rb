e887ae00fa451cfeb7569bbf1821b73644043412
jeferbc <jeffe.bernal@gmail.com>
2020-02-07 11:06:04 -0500

    solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :products
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :products, only: [:index]
+    end
+  end
 end
